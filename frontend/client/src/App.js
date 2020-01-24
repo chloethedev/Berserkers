79ff06436d111c98c489ed2c3114323506dbf986
chloethedev <chloethedev@gmail.com>
2020-01-24 17:49:30 -0500

    CHanged all of the const functions to async functions (most of them anyways). Now the info resets when users change.

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -6,7 +6,6 @@ function App() {
   const [web3, setWeb3] = useState(undefined);
   const [accounts, setAccounts] = useState(undefined);
   const [contract, setContract] = useState(undefined);
-  const [user, setUser] = useState(undefined)
   const [totalSupplyStake, setTotalSupplyStake] = useState(undefined);
   const [auctionStartTime0, setAuctionStartTime0] = useState(undefined);
   const [auctionStartTime1, setAuctionStartTime1] = useState(undefined);
@@ -23,6 +22,7 @@ function App() {
   const [playerVault, setPlayerVault] = useState(undefined);
   const [contractAddress, setContractAddress] = useState(undefined);
   const [userStake, setUserStake] = useState(undefined);
+  const [dividendsOwing, setDividendsOwing] = useState(undefined);
   const [unclaimedDividends, setUnclaimedDividends] = useState(undefined);
   const [totalDividendPoints, setTotalDividendPoints] = useState(undefined);
 
@@ -37,101 +37,17 @@ function App() {
         Berserkers.abi,
         deployedNetwork && deployedNetwork.address,
       );
-      const user = accounts[0];
       const contractAddress = deployedNetwork.address
 
       const totalSupplyStake = await contract.methods
         .totalSupplyStake()
         .call();
 
-      const auctionStartTime0 = await contract.methods
-        .auctionStartTime(0)
-        .call();
-      
-      const auctionStartTime1 = await contract.methods
-        .auctionStartTime(1)
-        .call();
-
-      const auctionStartTime2 = await contract.methods
-        .auctionStartTime(2)
-        .call();
-
-      const unitsLeftForSale0 = await contract.methods
-        .unitsLeftForSale(0)
-        .call();
-
-      const unitsLeftForSale1 = await contract.methods
-        .unitsLeftForSale(1)
-        .call();
-
-      const unitsLeftForSale2 = await contract.methods
-        .unitsLeftForSale(2)
-        .call();
-
-      const unitsPrice0  = web3.utils.fromWei(await contract.methods
-        .unitsPrice(0)
-        .call(), 'ether');
-
-      const unitsPrice1 = web3.utils.fromWei(await contract.methods
-        .unitsPrice(1)
-        .call(), 'ether');
-
-      const unitsPrice2 = web3.utils.fromWei(await contract.methods
-        .unitsPrice(2)
-        .call(), 'ether');
-
-      const unitsOwned0 = await contract.methods
-        .unitsOwned(accounts[0], 0)
-        .call();
-
-      const unitsOwned1 = await contract.methods
-        .unitsOwned(accounts[0], 1)
-        .call();
-
-      const unitsOwned2 = await contract.methods
-        .unitsOwned(accounts[0], 2)
-        .call();
-
-      const playerVault = web3.utils.fromWei(await contract.methods
-        .playerVault(accounts[0])
-        .call(), 'ether');
-
-      const userStake = await contract.methods
-        .stake(accounts[0])
-        .call();
-
-      const unclaimedDividends = web3.utils.fromWei(await contract.methods
-        .unclaimedDividends()
-        .call(), 'ether');
-
-      const totalDividendPoints = web3.utils.fromWei(await contract.methods
-        .totalDividendPoints()
-        .call(), 'ether')
-
-
-
       setWeb3(web3);
       setAccounts(accounts);
       setContract(contract);
-      setUser(user);
-      setTotalSupplyStake(totalSupplyStake);
-      setAuctionStartTime0(auctionStartTime0);
-      setAuctionStartTime1(auctionStartTime1);
-      setAuctionStartTime2(auctionStartTime2);
-      setUnitsLeftForSale0(unitsLeftForSale0);
-      setUnitsLeftForSale1(unitsLeftForSale1);
-      setUnitsLeftForSale2(unitsLeftForSale2);
-      setUnitsPrice0(unitsPrice0);
-      setUnitsPrice1(unitsPrice1);
-      setUnitsPrice2(unitsPrice2);
-      setUnitsOwned0(unitsOwned0);
-      setUnitsOwned1(unitsOwned1);
-      setUnitsOwned2(unitsOwned2);
-      setPlayerVault(playerVault);
       setContractAddress(contractAddress);
-      setUserStake(userStake);
-      setUnclaimedDividends(unclaimedDividends);
-      setTotalDividendPoints(totalDividendPoints);
+      setTotalSupplyStake(totalSupplyStake);
 
     }
     init();
@@ -139,12 +55,6 @@ function App() {
       setAccounts(accounts);
     });
     }, []);
-
-    useEffect(() => {
-      if(isReady()) {
-        // updateSoldiers();
-      }
-    }, [accounts, contract, web3,]);
 
   const isReady = () => {
     return (
@@ -154,11 +64,136 @@ function App() {
     );
   }
 
-  // async function updateUser() {
-  //   await contract.methods
-  //     .
-  // }
+    useEffect(() => {
+      if(isReady()) {
+        updateAuctionStartTime0();
+        updateAuctionStartTime1();
+        updateAuctionStartTime2();
+        updateUnitsLeftForSale0();
+        updateUnitsLeftForSale1();
+        updateUnitsLeftForSale2();
+        updateUnitsPrice0();
+        updateUnitsPrice1();
+        updateUnitsPrice2();
+        updateUnitsOwned0();
+        updateUnitsOwned1();
+        updateUnitsOwned2();
+        updatePlayerVault();
+        updateUserStake();
+        updateUnclaimedDividends();
+        updateTotalDividendPoints();
+        updateDividendsOwing();
 
+      }
+    }, [accounts, contract, web3,]);
+
+// UI FUNCTIONS
+
+  async function getPlayerInfo() {
+    await contract.methods
+      .getPlayerInfo(accounts[0])
+      .call();
+  }
+
+  async function getAuctionInfo() {
+    await contract.methods
+      .getAuctionInfo()
+      .call();
+  }
+
+  async function getBattlefieldInfo() {
+    await contract.methods
+      .getBattlefieldInfo()
+      .call
+  }
+
+  // AUCTION/UNIT FUNCTIONS
+
+  async function updateAuctionStartTime0() {
+    const auctionStartTime0 = await contract.methods
+      .auctionStartTime(0)
+      .call();
+    setAuctionStartTime0(auctionStartTime0);
+  }
+
+  async function updateAuctionStartTime1() {
+    const auctionStartTime1 = await contract.methods
+      .auctionStartTime(1)
+      .call();
+    setAuctionStartTime1(auctionStartTime1);
+  }
+
+  async function updateAuctionStartTime2() {
+    const auctionStartTime2 = await contract.methods
+      .auctionStartTime(2)
+      .call();
+    setAuctionStartTime2(auctionStartTime2);
+  }
+
+  async function updateUnitsLeftForSale0() {
+    const unitsLeftForSale0 = await contract.methods
+      .unitsLeftForSale(0)
+      .call();
+    setUnitsLeftForSale0(unitsLeftForSale0);
+  }
+
+  async function updateUnitsLeftForSale1() {
+    const unitsLeftForSale1 = await contract.methods
+      .unitsLeftForSale(1)
+      .call();
+    setUnitsLeftForSale1(unitsLeftForSale1);
+  }
+
+  async function updateUnitsLeftForSale2() {
+    const unitsLeftForSale2 = await contract.methods
+      .unitsLeftForSale(2)
+      .call();
+    setUnitsLeftForSale2(unitsLeftForSale2);
+  }
+
+  async function updateUnitsPrice0() {
+    const unitsPrice0 =  web3.utils.fromWei(await contract.methods
+      .unitsPrice(0)
+      .call(), 'ether');
+    setUnitsPrice0(unitsPrice0);
+  }
+
+  async function updateUnitsPrice1() {
+    const unitsPrice1 = web3.utils.fromWei(await contract.methods
+      .unitsPrice(1)
+      .call(), 'ether');
+    setUnitsPrice1(unitsPrice1);
+  }
+
+  async function updateUnitsPrice2() {
+    const unitsPrice2 = web3.utils.fromWei(await contract.methods
+      .unitsPrice(2)
+      .call(), 'ether');
+    setUnitsPrice2(unitsPrice2);
+  }
+
+  async function updateUnitsOwned0() {
+    const unitsOwned0 = await contract.methods
+      .unitsOwned(accounts[0], 0)
+      .call();
+    setUnitsOwned0(unitsOwned0);
+  }
+
+  async function updateUnitsOwned1() {
+    const unitsOwned1 = await contract.methods
+      .unitsOwned(accounts[0], 1)
+      .call();
+    setUnitsOwned1(unitsOwned1);
+  }
+
+  async function updateUnitsOwned2() {
+    const unitsOwned2 = await contract.methods
+      .unitsOwned(accounts[0], 2)
+      .call();
+    setUnitsOwned2(unitsOwned2);
+  }
+
+// SOLDIER BUYING BUTTONS
   async function DutchAuctionBuy(e) {
     e.preventDefault();
     await contract.methods
@@ -166,6 +201,9 @@ function App() {
       .send(
         {from:accounts[0]}
       );
+    await getPlayerInfo();
+    await getAuctionInfo();
+    await updateDividendsOwing();
   }
 
   async function DutchAuctionBuy1(e) {
@@ -175,6 +213,9 @@ function App() {
       .send(
         {from: accounts[0]}
       );
+    await getPlayerInfo();
+    await getAuctionInfo();
+    await updateDividendsOwing();
   }
 
   async function DutchAuctionBuy2(e) {
@@ -184,7 +225,50 @@ function App() {
       .send(
         {from: accounts[0]}
       );
+    await getPlayerInfo();
+    await getAuctionInfo();
+    await updateDividendsOwing();
   }
+
+// ATTACK
+  async function Attack(e) {
+    e.preventDefault();
+    const spot = e.target.elements[0].value;
+    const unitType = e.target.elements[1].value;
+    await contract.methods
+      .Attack(spot, unitType)
+      .send(
+        {from: accounts[0]}
+      );
+    await getBattlefieldInfo();
+    await updateUserStake();
+    await updateDividendsOwing(0);
+    await getPlayerInfo();
+    }
+
+
+  // VAULT STAKE  and DIV FUNCTIONS
+
+  async function updatePlayerVault() {
+    const playerVault = web3.utils.fromWei(await contract.methods
+      .playerVault(accounts[0])
+      .call(), 'ether');
+    setPlayerVault(playerVault);
+  } 
+
+  async function updateUserStake() {
+    const userStake = await contract.methods
+      .stake(accounts[0])
+      .call();
+    setUserStake(userStake);
+  } 
+
+  async function updateUnclaimedDividends() {
+    const unclaimedDividends = web3.utils.fromWei(await contract.methods
+      .unclaimedDividends()
+      .call(), 'ether');
+    setUnclaimedDividends(unclaimedDividends);
+  } 
 
   async function fillPlayerVault(e) {
     e.preventDefault();
@@ -196,25 +280,18 @@ function App() {
     })
     .then(function(reciept) {
       return reciept;
-    }))
+    }));
+    await getPlayerInfo();
     }
 
-  async function Attack(e) {
-    e.preventDefault();
-    const spot = e.target.elements[0].value;
-    const unitType = e.target.elements[1].value;
-    await contract.methods
-      .Attack(spot, unitType)
-      .send(
-        {from: accounts[0]}
-      );
-    }
+
   async function fetchdivs() {
     await contract.methods
       .fetchdivs(accounts[0])
       .send(
         {from: accounts[0]}
       );
+    await updateDividendsOwing(0);
   }
 
   async function vaultToWallet() {
@@ -223,25 +300,70 @@ function App() {
       .send(
         {from: accounts[0]}
       );
+    await updateDividendsOwing();
   }
 
+  async function updateTotalDividendPoints() {
+    const totalDividendPoints = web3.utils.fromWei(await contract.methods
+      .totalDividendPoints()
+      .call(), 'ether')
+    setTotalDividendPoints(totalDividendPoints);
+  }
+
+  async function updateDividendsOwing() {
+    const dividendsOwing = await contract.methods
+      .dividendsOwing(accounts[0])
+      .call();
+    setDividendsOwing(dividendsOwing);
+  }
 
   if (!isReady()) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container m-auto pb-5">
 
-        <h1 className="text-center">Welcome to Berserkers</h1>
-        <p className="text-center">Contract Address: {contractAddress}</p>
-        <h5 className="text-center">Your Address: {user}</h5>
-        <p className="text-center">Your have conquered {userStake} out of a possible {totalSupplyStake} battlefields.</p>
+    // NAVBAR
+<>
+    <div className="container">
 
-      <hr/>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">Berserkers</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">PlincHub</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Plinc Games</a>
+              </li>
+              <li className="nav-item mr-auto">
+                <a className="nav-link" href="#">Account: {accounts[0]}</a>
+              </li>
+            </ul>
+          </div>
+      </nav>
+
+    </div>
+
+    <br></br>
+
+  <div className="container m-auto pb-5">
+
+      <h1 className="text-center">Welcome to Berserkers</h1>
+      {/* <p className="text-center">Contract Address: {contractAddress}</p> */}
+      <p className="text-center">Your have conquered {userStake} out of a possible {totalSupplyStake} battlefields.</p>
+
+    <hr/>
 
       <div className="row">
-        <div className="col-4 text-center">Total Unclaimed Dividends: {unclaimedDividends} 
+        <div className="col-4 text-center">Your Unclaimed Dividends: {dividendsOwing} {unclaimedDividends}
         <br></br>
         <button type="submit" className="btn btn-primary text-center" onClick={e => fetchdivs(e)}>Fetch Divies!</button>
         </div>
@@ -269,18 +391,23 @@ function App() {
 
       <div className="row">
         <div className="col-12">
-          <form onSubmit={e => Attack(e)}>
-              <div className="form-group col-4">
+        <div className="col-8">
+            <p className="text-center float-right">THIS WILL SHOW THE BATTLEFIELDS</p>
+            
+        </div>
+          <form className="col-4" onSubmit={e => Attack(e)}>
+              <div className="form-group">
                 <label htmlFor="name">Battlefield Spot # (0-99)</label>
                 <input type="text" className="form-control" id="name" />
               </div>
-              <div className="form-group col-4">
+              <div className="form-group">
                 <label htmlFor="choices">Unit to attack with (0 - cav / 1 - sw / 2 - pike)</label>
                 <input type="text" className="form-control" id="choices" />
               </div>
-              <button type="submit" className="btn btn-primary col-4">ATTACK!</button>
-            </form>
+              <button type="submit" className="btn btn-primary">ATTACK!</button>
+          </form>
         </div>
+
       </div>
 
       <hr/>
@@ -328,6 +455,7 @@ function App() {
         </div>
       </div>
    </div>
+   </>
   );
 }
 
